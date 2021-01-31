3a63f49d67dfaa1a376e073f0f6cd3f0cd3e8c33
= <bradboratto@gmail.com>
2021-01-31 04:31:07 -0500

    q works

--- front/input.js
+++ front/input.js
@@ -1,6 +1,6 @@
 document.addEventListener('keydown', (e) => {
     var spd = {"w": -1, "s": 1, "q": 0}[e.key]
-    if(spd) ws.send(JSON.stringify({
+    if(spd != undefined) ws.send(JSON.stringify({
         "event": "input", "spd": spd
     }))
 })
