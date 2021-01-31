5d15176b6e73f4c75eabf1be0d8a78102fe59867
= <bradboratto@gmail.com>
2021-01-30 23:27:33 -0500

    debug values

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const ws = require('ws')
+
+// simple file server from /front/
+const app = express()
+app.use(express.static('front'))
+app.listen(80, () => console.log('up!'))
+
+const tickRate = 5
+const gameTick = () => {
+    const randomTen = e => Math.round(Math.random() * 10) / 10
+    return JSON.stringify({
+        "event": "gameTick",
+        "p1": { "y": randomTen() }, 
+        "p2": { "y": randomTen() }
+    })
+}
+
+const server = new ws.Server({ port: 81 }) 
+server.on('connection', socket => {
+    socket.on('message', () => {
+    })
+    socket.send(JSON.stringify({"event": "debug", "message": "hello, world!"}))
+    setInterval(() => 
+        socket.send(gameTick()), 
+    1000/tickRate)
+})
--- front/server.js
+++ front/server.js
@@ -1,20 +1,32 @@
 const ws = new WebSocket("ws://localhost:81")
 
-ws.addEventListener('connected', () => {
+const tickRate = 5
+var debug = false
+
+ws.addEventListener('open', () => {
     console.log('socket connected')
+    setInterval(serverTick, 1000/tickRate)
 })
 
 ws.addEventListener('message', (response) => {
     var message = JSON.parse(response.data)
-    console.log(message)
-    if(message.type = 'message') {
-        // update splash text
+
+    if(message.type == 'debug' || debug) {
+        console.log(message)
+    }
+    if(message.type == 'message' || debug) {
         text += message.content + '\n'
     }
-    if(message.type = 'gameTick') {
+    if(message.type == 'gameTick') {
         // update game if necessary
     }
-    if(message.type = 'start') {
+    if(message.type == 'start') {
         // start the game
     }
 })
+
+var tick = 0
+const serverTick = () => {
+    tick++
+    // ws.send(JSON.stringify({ tick: tick }))
+}
