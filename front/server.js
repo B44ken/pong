ec57a250fb71efabadebf45f2862def8316a73c5
= <bradboratto@gmail.com>
2021-01-31 01:17:22 -0500

    woooo user input

--- front/server.js
+++ front/server.js
@@ -4,23 +4,27 @@ const tickRate = 5
 var debug = false
 
 ws.addEventListener('open', () => {
-    console.log('socket connected')
     setInterval(serverTick, 1000/tickRate)
 })
 
+var ID = 0
 ws.addEventListener('message', (response) => {
     var message = JSON.parse(response.data)
 
-    if(message.type == 'debug' || debug) {
+    if(message.event == 'connect') {
+        ID = message.ID
+    }
+
+    if(message.event == 'debug' || debug) {
         console.log(message)
     }
-    if(message.type == 'message' || debug) {
+    if(message.event == 'message' || debug) {
         text += message.content + '\n'
     }
-    if(message.type == 'gameTick') {
+    if(message.event == 'gameTick') {
         // update game if necessary
     }
-    if(message.type == 'start') {
+    if(message.event == 'start') {
         // start the game
     }
 })
