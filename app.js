e51d3ca6c5a28acd32c993282a0eb8ee2996f1a0
= <bradboratto@gmail.com>
2021-01-31 01:18:49 -0500

    user input bby

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const app = express()
 app.use(express.static('front'))
 app.listen(80, () => console.log('up!'))
 
-const tickRate = 5
+var players = []
+
 const gameTick = () => {
+    
     const randomTen = e => Math.round(Math.random() * 10) / 10
     return JSON.stringify({
         "event": "gameTick",
-        "p1": { "y": randomTen() }, 
-        "p2": { "y": randomTen() },
+        "players": [
+            (players[0]), (players[1] || { "y": 0.5, "spd": 0 })
+        ],
         "ball": { 
             "x": randomTen(), 
             "y": randomTen()
@@ -21,10 +24,24 @@ const gameTick = () => {
 }
 
 const server = new ws.Server({ port: 81 }) 
+
+var tickRate = 4
 server.on('connection', socket => {
-    socket.on('message', () => {
+    if(players.length < 2) {
+        players.push({y: 0.5, spd: 0, socket: 'connected'})
+        var ID = players.length - 1
+    } else { ID = null }
+
+    console.log(players)
+    
+    socket.on('message', (text) => {
+        message = JSON.parse(text)
+        if(message.event == "input") {
+            players[ID].spd = message.spd
+        }
     })
-    socket.send(JSON.stringify({"event": "debug", "message": "hello, world!"}))
+    socket.send(JSON.stringify({"event": "connect", "ID": ID})) 
+    socket.send(JSON.stringify({"event": "debug", "content": "id: " + ID})) 
     setInterval(() => 
         socket.send(gameTick()), 
     1000/tickRate)
